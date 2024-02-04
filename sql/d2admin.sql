edec9689131484dedccb4edd28a139193ad11cf2
Odboy <tianjun@odboy.cn>
2024-02-04 20:18:17 +0800

    update sql data

--- sql/d2admin.sql
+++ sql/d2admin.sql
@@ -11,11 +11,40 @@
  Target Server Version : 80025 (8.0.25)
  File Encoding         : 65001
 
- Date: 04/02/2024 19:17:05
+ Date: 04/02/2024 20:16:51
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for system_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `system_menu`;
+CREATE TABLE `system_menu` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `menu_title` varchar(255) NOT NULL,
+  `menu_icon` varchar(255) NOT NULL DEFAULT '',
+  `menu_path` varchar(255) NOT NULL,
+  `router_path` varchar(255) NOT NULL,
+  `router_name` varchar(255) NOT NULL,
+  `router_auth` int NOT NULL,
+  `router_hidden` int NOT NULL,
+  `router_component_path` varchar(255) NOT NULL,
+  `menu_parent_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_system_menu_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of system_menu
+-- ----------------------------
+BEGIN;
+INSERT INTO `system_menu` (`id`, `created_at`, `updated_at`, `deleted_at`, `menu_title`, `menu_icon`, `menu_path`, `router_path`, `router_name`, `router_auth`, `router_hidden`, `router_component_path`, `menu_parent_id`) VALUES (1, '2024-02-04 20:01:59', '2024-02-04 20:02:02', NULL, '页面1', '', '/page1', 'page1', 'page1', 1, 0, 'demo/page1', NULL);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for system_router
@@ -75,6 +104,8 @@ CREATE TABLE `system_user` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
+  `uuid` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_system_user_deleted_at` (`deleted_at`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -83,7 +114,7 @@ CREATE TABLE `system_user` (
 -- Records of system_user
 -- ----------------------------
 BEGIN;
-INSERT INTO `system_user` (`id`, `username`, `password`, `created_at`, `updated_at`, `deleted_at`) VALUES (1, 'admin', '123456', '2024-02-04 13:17:00', '2024-02-04 13:17:03', NULL);
+INSERT INTO `system_user` (`id`, `username`, `password`, `created_at`, `updated_at`, `deleted_at`, `uuid`, `name`) VALUES (1, 'admin', '123456', '2024-02-04 13:17:00', '2024-02-04 13:17:03', NULL, 'admin', 'admin');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
